09c9cedcd83dbfc9734dc985d89b03e1d8bf52f7
Joy Chela <joychela000@gmail.com>
2024-09-09 12:34:16 +0300

    SQL

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES 
+    ("Mr. Chocolate", 20, "M", "Brown", "Calm", 1),
+    ("Rowdy", 15, "M", "Black", "Aggressive", 1),
+    ("Tabitha", 6, "F", "Honey", "Friendly", 1),
+    ("Sergeant Brown", 19, "M", "Brown", "Calm", 0),
+    ("Melissa", 13, "F", "Black", "Calm", 1),
+    ("Grinch", 2, "M", "White", "Grumpy", 1),
+    ("Wendy", NULL, "F", "Black", "Friendly", 0),
+    (NULL, NULL, "M", NULL, "Wild", 0);
